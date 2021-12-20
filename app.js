133767d199faaf9f18948bfd0d93c409e9c6f7df
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-12-20 01:27:54 +0000

    contact form submit and contact-success view done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
@@ -10,6 +13,10 @@ app.get('/', function(req,res) {
 
 app.get('/contact', function (req, res) {
   res.render('contact', {qs: req.query});
+})
+
+app.post('/contact', function (req, res) {
+  res.render('contact-success', { data: req.body });
 })
 
 app.get('/profile/:name', function (req, res) {
