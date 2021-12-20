88b184a34819aa13aacd688bfd454482f9cc1169
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-12-20 00:07:13 +0000

    serving static styles.css file using express

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req,res) {
   res.render('index');
